fb8431ea2420f629f2e72d05b01a56c2880eb30c
Saravanan Shankar <saravanan@chosencaregroup.com>
2020-05-05 14:14:51 +0530

    to be added

---
+++ src/components/TodoList.vue
@@ -0,0 +1,157 @@
+<template>
+  <div class="hello">
+    <input
+      type="text"
+      class="todo-input"
+      placeholder="What need to be done"
+      v-model="newTodo"
+      @keyup.enter="addTodo"
+    />
+    <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
+      <div class="todo-item-left">
+        <div
+          v-if="!todo.editing"
+          @dblclick="editTodo(todo)"
+          class="todo-item-label"
+        >
+          {{ todo.title }}
+        </div>
+        <input
+          v-else
+          class="todo-item-edit"
+          type="text"
+          v-model="todo.title"
+          @blur="doneEdit(todo)"
+          @keyup.enter="doneEdit(todo)"
+          @keyup.esc="cancelEdit(todo)"
+          v-focus
+        />
+      </div>
+      <div class="remove-item" @click="removeTodo(index)">
+        &times;
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      newTodo: "",
+      idForTodo: 4,
+      beforeEditCache: "",
+      todos: [
+        {
+          id: 1,
+          title: "Task 1 : Design the business logic",
+          completed: false,
+          editing: false
+        },
+        {
+          id: 2,
+          title: "Task 2 : Design the Website",
+          completed: false,
+          editing: false
+        },
+        {
+          id: 3,
+          title: "Task 3 : Implement the Design",
+          completed: false,
+          editing: false
+        }
+      ]
+    };
+  },
+  directives: {
+    focus: {
+      inserted: function(el) {
+        el.focus();
+      }
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim().length == 0) {
+        return;
+      }
+
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        completed: false
+      });
+      this.newTodo = "";
+      this.idForTodo++;
+    },
+    editTodo(todo) {
+      this.beforeEditCache = todo.title;
+      todo.editing = true;
+    },
+    doneEdit(todo) {
+      if (todo.title.trim() == "") {
+        todo.title = this.beforeEditCache;
+      }
+      todo.editing = false;
+    },
+    cancelEdit(todo) {
+      todo.title = this.beforeEditCache;
+      todo.editing = false;
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+.todo-input {
+  width: 100%;
+  padding: 10px 18px;
+  font-size: 18px;
+  margin-bottom: 16px;
+  &:focus {
+    outline: 0;
+  }
+}
+.todo-item {
+  margin-bottom: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.remove-item {
+  cursor: pointer;
+  margin-left: 14px;
+  &:hover {
+    color: red;
+  }
+}
+.todo-item-left {
+  display: flex;
+  align-items: center;
+}
+.todo-item-label {
+  padding: 10px;
+  border: Solid 1px #fff;
+  margin-left: 12px;
+}
+.todo-item-edit {
+  font-size: 24px;
+  color: #2c3e50;
+  margin-left: 12px;
+  width: 100%;
+  padding: 10px;
+  border: Solid 1px #ccc;
+  font-family: "Avenir", Arial, Helvetica, sans-serif;
+  &:focus {
+    outline: 0;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Vue logo" class="logo" src="../assets/logo.png" />
+
+    <todo-list></todo-list>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//import HelloWorld from "@/components/HelloWorld.vue";
+import TodoList from "@/components/TodoList.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    TodoList
   }
 };
 </script>
